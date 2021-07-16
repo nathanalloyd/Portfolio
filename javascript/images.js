5c5a14e3233247e7eb357766aa063356b073b604
Nathan Lloyd <nathanalloyd@gmail.com>
2021-07-16 18:21:30 -0400

    add media queries for large screens and add button functionality.

--- javascript/images.js
+++ javascript/images.js
@@ -1,38 +1,14 @@
-const $element = ('.fix');
-const imagePath = '/assets';
-const totalFrames = 4;
-const animationDuration = 500;
-const timePerFrame = animationDuration / totalFrames;
-let timeWhenLastUpdate;
-let timeFromLastUpdate;
-let frameNumber = 1;
-
-function step(startTime) {
-  if (!timeWhenLastUpdate) timeWhenLastUpdate = startTime;
-
-  timeFromLastUpdate = startTime - timeWhenLastUpdate;
-
-  if (timeFromLastUpdate > timePerFrame) {
-    $element.attr('src', imagePath + '/avatartext${frameNumber}.png');
-    timeWhenLastUpdate = startTime;
-
-    if (frameNumber >= totalFrames) {
-      frameNumber = 1;
-    } else {
-       frameNumber = frameNumber + 1;
-    }        
-  }
-
-  requestAnimationFrame(step);
+const homefunction = () => {
+    document.getElementById("bio").style.display ='none'
+    document.getElementById("projects").style.display ='none'
 }
 
-$(document).ready(() => {
-    for (var i = 1; i < totalFrames + 1; i++) {
-      $('body').append(`<div id="preload-image-${i}" style="background-image: url('${imagePath}/avatartext${i}.png');"></div>`);
-    }
-  });
-  
-  // wait for images to be downloaded and start the animation
-  $(window).on('load', () => {
-    requestAnimationFrame(step);
-  });
+const biofunction = () => {
+    document.getElementById("bio").style.display ='block'
+    document.getElementById("projects").style.display ='none'
+}
+
+const projectsfunction = () => {
+    document.getElementById("projects").style.display ='block'
+    document.getElementById("bio").style.display ='none'
+}
