da7ae9fc7cab5cf9d74fbad2fba527109aa3aeaa
Nathan Lloyd <nathanalloyd@gmail.com>
2021-07-16 20:27:14 -0400

    add several images and javascript code to show or hide images.

--- javascript/images.js
+++ javascript/images.js
@@ -1,14 +1,29 @@
-const homefunction = () => {
-    document.getElementById("bio").style.display ='none'
-    document.getElementById("projects").style.display ='none'
-}
-
 const biofunction = () => {
     document.getElementById("bio").style.display ='block'
+    document.getElementById("skills").style.display ='none'
     document.getElementById("projects").style.display ='none'
+    document.getElementById("avatar1").style.display ='none'
+    document.getElementById("avatar2").style.display ='block'
+    document.getElementById("avatar3").style.display ='none'
+    document.getElementById("avatar4").style.display ='none'
+}
+
+const skillsfunction = () => {
+    document.getElementById("skills").style.display ='block'
+    document.getElementById("bio").style.display ='none'
+    document.getElementById("projects").style.display ='none'
+    document.getElementById("avatar1").style.display ='none'
+    document.getElementById("avatar2").style.display ='none'
+    document.getElementById("avatar3").style.display ='block'
+    document.getElementById("avatar4").style.display ='none'
 }
 
 const projectsfunction = () => {
     document.getElementById("projects").style.display ='block'
     document.getElementById("bio").style.display ='none'
+    document.getElementById("skills").style.display ='none'
+    document.getElementById("avatar1").style.display ='none'
+    document.getElementById("avatar2").style.display ='none'
+    document.getElementById("avatar3").style.display ='none'
+    document.getElementById("avatar4").style.display ='block'
 }
